bcb189fca12e1b2db221fa1050b1708760c6d99f
fabioferre <fabio.ferreira@w1finance.com.br>
2018-07-21 21:23:05 -0300

    Solução

---
+++ build/navbar.php
@@ -0,0 +1,66 @@
+<header class="container-fluid bg-default" style="border-top: 1px solid #dddddd; padding-top: -10px;">
+	<div class="col-md-12 pt-2 ">
+		<a class="navbar-brand " href="#"><img src="/<?=pasta?>/assets/img/logo_site.png" style=""></a>
+	</div>
+</header>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-default">
+	<div class="col-md-2"></div>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHome" aria-controls="navbarHome" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse ml-4" id="navbarHome">
+        <ul class="navbar-nav mr-auto" style="margin-left: 4px;">
+
+            <li class="nav-item">
+                <a class="nav-link" href="http://novoterralima.com/empresa/" >
+                   sobre nós
+                </a>
+            </li>
+
+            
+			<li class="nav-item dropdown ">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					portfólio
+				</a>
+				<div class="dropdown-menu dropdown-menu1" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item dropdown-item1 " href="#">locação</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item dropdown-item1" href="#">venda</a>
+				</div>
+			</li>
+
+            <li class="nav-item ">
+                <a class="nav-link" href="#">
+                    corretores
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    fale conosco
+                </a>
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="#" >
+                    cadastre seu imóvel
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#" >
+                    mapa
+                </a>
+            </li>
+
+            <li class="nav-item ">
+                <a class="nav-link" href="#" >
+                    blog
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
